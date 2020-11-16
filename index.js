209ca27e475300d12d26ba727aa87935dae7c6bf
Mai Thanh Danh <mtd.maithanhdanh@gmail.com>
2020-11-16 23:28:51 +0700

    [UPDATE] re-order logger when server on

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const { checkHealth } = require("./utils/checkHealth")
 mongoose.connect()
 
 server.listen(ENV_VAR.PORT, () => {
+	logger.info(`Server is running on port ${ENV_VAR.PORT} (${ENV_VAR.NODE_ENV})`)
 	if (ENV_VAR.NODE_ENV !== "test") {
 		setInterval(() => checkHealth(), 5 * 60 * 1000)
 		checkHealth()
 	}
-	logger.info(`Server is running on port ${ENV_VAR.PORT} (${ENV_VAR.NODE_ENV})`)
 })
 
 module.exports = server
